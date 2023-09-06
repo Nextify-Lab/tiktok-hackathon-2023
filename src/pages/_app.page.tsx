fcb7e8cd54d780bd8b616902d5eb9b79a046a300
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-09-06 10:24:42 +0800

    chakra ui setup

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import theme from "@/theme";
 
+// SOMETHING TO NOTE HERE, IF U USE CHAKRA UI COMPONENT, DONT USE TAILWIND ON THAT COMPONENT
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    tikTokBlack: "#121212",
+    tikTokWhite: "#ffffff",
+  },
+});
+
+export default theme;
