cfa329389b9cea0a6eba405e8b22187f419f164c
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-09-06 11:53:32 +0800

    setup firebase config and read sample

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+
+  env: {
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+  },
 };
 
 module.exports = nextConfig;
