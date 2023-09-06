02082d15e81abc52aa903ad57eef68e46a398108
a-nnza-r <ansarahmedcjc17@gmail.com>
2023-09-06 23:55:52 +0800

    Buyer routes completed

---
+++ src/pages/api/buyers/route.page.ts
@@ -0,0 +1,73 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../firebase/admin";
+
+const buyersCollection = db.collection("buyers");
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method, query } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        if (Object.keys(query).length === 0) {
+          res.status(400).json({ error: "No search parameters provided" });
+          return;
+        }
+
+        let queryRef: FirebaseFirestore.Query<FirebaseFirestore.DocumentData> =
+          buyersCollection;
+        for (const key in query) {
+          if (key === "age") {
+            const ageValue = parseInt(query[key] as string, 10); // Convert to base 10
+            if (!isNaN(ageValue)) {
+              // Check if the conversion was successful
+              queryRef = queryRef.where(key, "==", ageValue);
+            } else {
+              res.status(400).json({ error: "Invalid age parameter" });
+              return;
+            }
+          } else {
+            // For other query parameters, you can proceed as before
+            queryRef = queryRef.where(key, "==", query[key]);
+          }
+        }
+
+        const querySnapshot = await queryRef.get();
+        const buyers = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        res.status(200).json(buyers);
+      } catch (error: any) {
+        console.log(error);
+        res.status(500).json({ error: error.toString() });
+      }
+      break;
+
+    case "POST":
+      try {
+        const newBuyer: Buyer = req.body; // Assuming req.body contains the Buyer object
+        // Check if the username already exists
+        const usernameExistsQuery = await buyersCollection
+          .where("username", "==", newBuyer.username)
+          .get();
+
+        if (!usernameExistsQuery.empty) {
+          res.status(400).json({ error: "Username already exists" });
+          return;
+        }
+
+        // If the username is unique, add the newBuyer to the database
+        const docRef = await buyersCollection.add(newBuyer);
+        const createdBuyer = { ...newBuyer, id: docRef.id }; // Set id separately
+        res.status(201).json(createdBuyer);
+      } catch (error: any) {
+        res.status(500).json({ error: error.toString() });
+      }
+      break;
+  }
+}
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,103 @@
-interface SampleType {
-  sample: string;
+class Buyer {
+  id: string;
+  username: string;
+  age: number;
+  address: string;
+  deleted: boolean;
+
+  constructor(
+    id: string,
+    username: string,
+    age: number,
+    address: string,
+    deleted: boolean
+  ) {
+    this.id = id;
+    this.username = username.trim();
+    this.age = age;
+    this.address = address.trim();
+    this.deleted = deleted;
+  }
+}
+
+class Shop {
+  id: string;
+  tiktokUsername: string;
+  groupBuys: GroupBuy[];
+  items: Item[];
+  rating: number;
+
+  constructor(
+    id: string,
+    tiktokUsername: string,
+    groupBuys: GroupBuy[],
+    items: Item[],
+    rating: number
+  ) {
+    this.id = id;
+    this.tiktokUsername = tiktokUsername.trim(); // Sanitize the tiktokUsername by removing leading/trailing whitespaces
+    this.groupBuys = groupBuys;
+    this.items = items;
+    this.rating = rating;
+  }
+}
+
+class Item {
+  productName: string;
+  shop: Shop;
+  buyer: Buyer;
+  stock: number;
+  price: number;
+  description: string;
+
+  constructor(
+    productName: string,
+    shop: Shop,
+    buyer: Buyer,
+    stock: number,
+    price: number,
+    description: string
+  ) {
+    this.productName = productName.trim(); // Sanitize the productName by removing leading/trailing whitespaces
+    this.shop = shop;
+    this.buyer = buyer;
+    this.stock = Math.abs(stock); // Ensure stock is a positive value
+    this.price = Math.abs(price); // Ensure price is a positive value
+    this.description = description;
+  }
+}
+
+interface GroupBuy {
+  id: string;
+  buyers: Buyer[];
+  shop: Shop;
+  messages?: Message[]; // Optional
+}
+
+class GroupBuy {
+  id: string;
+  buyers: Buyer[];
+  shop: Shop;
+  messages?: Message[]; // Optional
+
+  constructor(id: string, buyers: Buyer[], shop: Shop, messages?: Message[]) {
+    this.id = id;
+    this.buyers = buyers;
+    this.shop = shop;
+    this.messages = messages;
+  }
+}
+
+class Message {
+  time: Date;
+  sender: string;
+  receiver: string;
+  msgStr: string;
+
+  constructor(time: Date, sender: string, receiver: string, msgStr: string) {
+    this.time = time;
+    this.sender = sender.trim(); // Sanitize the sender by removing leading/trailing whitespaces
+    this.receiver = receiver.trim(); // Sanitize the receiver by removing leading/trailing whitespaces
+    this.msgStr = msgStr.trim(); // Sanitize the message by removing leading/trailing whitespaces
+  }
 }
