8eef5251195cfb4f2e4ef3f2f41a3db967fcc7e2
NathanAW24 <nathan_wiryawan@mymail.sutd.edu.sg>
2023-09-08 01:36:38 +0800

    order summary barebones

--- src/components/ShopFlow/OrderSummary/OrderSummary.tsx
+++ src/components/ShopFlow/OrderSummary/OrderSummary.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
 
 const OrderSummary = () => {
-  return (
-    <div>OrderSummary</div>
-  )
-}
+  // Mocked values for now
+  const subtotal = 100; // replace with the actual value later
+  const shipping = 10; // replace with the actual value later
+  const total = subtotal + shipping;
 
-export default OrderSummary
+  return (
+    <Box p={4} boxShadow="md" borderWidth="1px" mb={4}>
+      {/* First Row */}
+      <Text mb={3} fontWeight="bold" fontSize="xl">
+        Order summary
+      </Text>
+
+      {/* Second Row */}
+      <Flex justifyContent="space-between" mb={2}>
+        <Text>Subtotal</Text>
+        <Text>S${subtotal.toFixed(2)}</Text>
+      </Flex>
+
+      {/* Third Row */}
+      <Flex justifyContent="space-between" mb={2}>
+        <Text>Shipping</Text>
+        <Text>S${shipping.toFixed(2)}</Text>
+      </Flex>
+
+      {/* Fourth Row */}
+      <Flex justifyContent="space-between">
+        <Text fontWeight="bold">Total</Text>
+        <Text fontWeight="bold">S${total.toFixed(2)}</Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default OrderSummary;
